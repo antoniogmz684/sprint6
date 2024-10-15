b0b85ad5e03f553bf1a6242407462af51abc19a2
antoniogmz684 <antoniogmz684@gmail.com>
2024-10-15 16:44:46 -0600

    Inclusión de header

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+#Header incluido
+st.header("Análisis de datos de vehículos")
+
+#variables para los checkboxes
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_cbox = st.checkbox('Construir histograma') #Crea un botón
 disp_cbox = st.checkbox('Construir gráfico de dispersión')
 
+
+#creación de las gráficas al seleccionar en los checboxes
 if hist_cbox: #Al hacer click en el botón
     #Escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
